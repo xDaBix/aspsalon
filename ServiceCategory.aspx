66021bd6f6b22539caa451b37db53e7e21d2116d
nupursachan <145092198+nupursachan@users.noreply.github.com>
2024-04-09 14:53:44 +0530

    Add files via upload

---
+++ ServiceCategory.aspx
@@ -0,0 +1,76 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ServiceCategory.aspx.cs" Inherits="SalonProject.admin.ServiceCategory" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+   <title>Insert Service Category</title>
+    <style>
+
+        h2{
+            text-align:center;
+        }
+     
+        .form-container {
+            margin-top:7%;
+            width: 50%;
+            margin-left:24%;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 10px;
+        }
+        .form-group {
+            margin-bottom: 15px;
+        }
+        .form-group label {
+            display: block;
+            margin-bottom: 5px;
+        }
+        .form-group input[type=text], .form-group select {
+            width: 100%;
+            padding: 8px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+        .btnadd {
+            background-color: #331414;
+            color: white;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            margin-left:45%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+      <div class="form-container">
+            <h2>Add Service Category</h2><br /><br />
+           
+            <div class="form-group">
+                <label for="txtCategoryName">Enter Service Categor:</label>
+                <asp:TextBox ID="txtCategoryName" runat="server"    pattern="^[a-zA-Z\s]+$" required="true" ></asp:TextBox>
+                 <div>
+                      <asp:Label ID="errname" runat="server" ForeColor="Red"></asp:Label>
+                    </div>
+            </div><br />
+            <div class="form-group">
+                <label for="ddlCategoryType">Select Service Type:</label>
+                <asp:DropDownList ID="ddlCategoryType" runat="server" required="true">
+                     <asp:ListItem Value="" Text="Select Type" Disabled="true" Selected="true"></asp:ListItem>
+                    <asp:ListItem Text="Male" Value="Male"></asp:ListItem>
+                    <asp:ListItem Text="Female" Value="Female"></asp:ListItem>
+                 
+                </asp:DropDownList>
+            </div><br /><br />
+
+           <asp:Button ID="add" class="btnadd" Text="Add" runat="server" OnClick="AddClick" />
+           <div>
+                        <asp:Label ID="txterror" runat="server" ForeColor="Red"></asp:Label>
+                    </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ServiceCategory.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SalonProject.admin
+{
+    public partial class ServiceCategory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+              
+                ClearFields();
+            }
+        }
+
+        protected void AddClick(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=NUPUR;Initial Catalog=salon;Integrated Security=True";
+
+
+            String categoryName = "";
+            bool value = true;
+            if (!string.IsNullOrEmpty(txtCategoryName.Text.Trim()))
+            {
+
+                string inputCategoryName = txtCategoryName.Text.Trim().ToLower();
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    string qry = "SELECT * FROM ServiceCategory WHERE LOWER(CategoryName) = @name and CType=@type" ;
+                    using (SqlCommand cmd = new SqlCommand(qry, con))
+                    {
+                        cmd.Parameters.AddWithValue("@name", txtCategoryName.Text.Trim());
+                        cmd.Parameters.AddWithValue("@type", ddlCategoryType.SelectedValue);
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+
+                                value = false;
+                                errname.Text = "Category is already Added";
+                                
+                            }
+                            else
+                            {
+
+                                categoryName = txtCategoryName.Text.Trim();
+                               
+                            }
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                value = false;
+                errname.Text = "enter valid Category";
+            }
+
+         
+          
+
+
+
+            if (value == true)
+            {
+
+                string query = "INSERT INTO ServiceCategory (CategoryName, CType) VALUES (@CategoryName, @CategoryType)";
+
+
+
+
+
+
+
+                try
+                {
+
+                    string categoryType = ddlCategoryType.SelectedValue;
+
+
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        using (SqlCommand command = new SqlCommand(query, connection))
+                        {
+
+
+                            command.Parameters.AddWithValue("@CategoryName", categoryName);
+                            command.Parameters.AddWithValue("@CategoryType", categoryType);
+
+                            connection.Open();
+                            command.ExecuteNonQuery();
+                            HttpContext.Current.Response.Write("<script>alert('Category Add Successfullyy!!!!!');</script>");
+                        }
+                    }
+                    ClearFields();
+
+                }
+                catch (Exception ex)
+                {
+                    txterror.Text = "Error: " + ex.Message;
+                }
+
+            }
+           
+
+        }
+
+        private void ClearFields()
+        {
+           
+            txtCategoryName.Text = "";
+            ddlCategoryType.SelectedIndex = 0;
+            errname.Text = "";
+            txterror.Text = "";
+        }
+
+    }
+}
